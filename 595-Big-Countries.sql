714fe787d8f9eb1b2c4e973d16be8a60a4cd794b
Satadru mondal <166845471+SATADRU1@users.noreply.github.com>
2026-09-07 21:21:51 +0530

    [595] [Time Beats: 27.16%] - LeetPush

--- 595-Big-Countries.sql
+++ 595-Big-Countries.sql
@@ -1,6 +1,5 @@
--- Write your PostgreSQL query statement below
-
-select name, population,area from World
-where 
-area >= 3000000 or 
-population >= 25000000
+# Write your MySQL query statement below
+select name , population , area 
+from World
+where population >= 25000000 or
+area >= 3000000 
